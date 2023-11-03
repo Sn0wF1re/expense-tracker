bf335233814312f20f7417a1caef3d9a70697c07
Sn0wF1re <mokayaclinton34@gmail.com>
2023-11-03 11:56:48 +0300

    Create new project using vite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,30 @@
-<template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-    <footer>&copy Sn0wF1re. All rights reserved {{ date }}</footer>
-  </div>
-</template>
-
-<script>
-import { ref } from 'vue';
-  export default {
-    setup() {
-      const date = ref(null);
-
-      date.value = new Date().getFullYear();
-
-      return { date };
-    }
-  }
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-nav {
-  padding: 30px;
-  background: #000;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-nav a {
-  font-weight: bold;
-  color: #cbd2da;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-footer {
-  background-color: black;
-  color: white;
-  bottom: 0;
-  padding: 1.5rem;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
