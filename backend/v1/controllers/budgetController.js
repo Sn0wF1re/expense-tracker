5f93db373e6339e22154616c33f8dcc7f9e38f81
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-07 21:37:33 +0300

    Create controllers for budget model

---
+++ backend/v1/controllers/budgetController.js
@@ -0,0 +1,91 @@
+const Budget = require('../models/budget');
+
+// create budget
+const createBudget = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const newBudget = await Budget.create({
+      userId: user.id,
+      budget: req.body.budget,
+    });
+    res.json(newBudget);
+  } catch(err) {
+    next(err);
+  }
+};
+
+// get all budgets
+const getBudgets = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const budgets = await Budget.find({ userId });
+    res.json(budgets);
+  } catch(err) {
+    next(err);
+  }
+};
+
+// get one budget
+const getBudget = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const budgetId = req.params.id;
+    const budget = await Budget.findOne({ userId: user.id, _id: budgetId });
+    if (budget) {
+      res.json(budget)
+    } else {
+      res.status(404).json({ message: 'Budget not found!' })
+    }
+  } catch(err) {
+    next(err);
+  }
+};
+
+// update a budget
+const updateBudget = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const budgetId = req.params.id;
+    const update = {
+      budget: req.body.budget,
+    }
+    const budget = await Budget.findOneAndUpdate({
+      userId,
+      _id: budgetId
+    },
+    update,
+    { new: true }
+    );
+    if (budget) {
+      res.json(budget);
+    } else {
+      res.status(404).json({ message: 'Budget not found!' });
+    }
+  } catch(err) {
+    next(err);
+  }
+};
+
+// delete a budget
+const deleteBudget = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const budgetId = req.params.id;
+    const budget = await Budget.findOneAndDelete({ userId, _id: budgetId });
+    if (budget) {
+      res.json(budget);
+    } else {
+      res.status(404).json({ message: 'Budget not found!' });
+    }
+  } catch(err) {
+    next(err);
+  }
+};
+
+module.exports = {
+    createBudget,
+    getBudgets,
+    getBudget,
+    updateBudget,
+    deleteBudget
+};
