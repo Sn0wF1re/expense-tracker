8a89d524e47d78641a33addb5c3b6434d960bc11
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-07 21:25:10 +0300

    Create layout for all pages

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,61 @@
+<template>
+    <header class="header">
+        <h2>WapiDoh</h2>
+
+        <nav class="navigation">
+            <a>Expenses</a>
+            <a>Categories</a>
+        </nav>
+        <a class="logout" @click="logout">Log Out</a>
+    </header>
+
+    <router-view />
+
+    <footer class="footer">
+        <p>&copy; {{ date }} WapiDoh</p>
+    </footer>
+</template>
+
+<script setup>
+import { useAuthStore } from '../stores/authStore';
+
+const authStore = useAuthStore();
+const date = new Date().getFullYear();
+const logout = () => {
+    authStore.logout();
+};
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1rem;
+    margin-bottom: 0;
+    padding: 0.5rem;
+
+    .navigation {
+        display: flex;
+        gap: 1rem;
+    }
+
+    .logout {
+        color: #E06336;
+        text-decoration: none;
+        cursor: pointer;
+    }
+}
+
+.footer {
+    display: flex;
+    justify-content: center;
+    margin: 1rem;
+    padding: 1rem 0.5rem;
+    /* border-top: 1px solid #E06336; */
+
+    p {
+        margin: 0;
+    }
+}
+</style>
