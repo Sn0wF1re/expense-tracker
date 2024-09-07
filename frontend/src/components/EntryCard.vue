1a743fd7b8577396d35def0007a340fd2161c308
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-07 21:26:16 +0300

    Create card to display expense entries

--- frontend/src/components/EntryCard.vue
+++ frontend/src/components/EntryCard.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="entry">
         <div class="entry-header">
-            <h2>{{ title }}</h2>
+            <div class="description">
+                <h2>
+                    {{ description }}
+                </h2>
+
+                <div class="actions">
+                    <q-icon name="edit_sharp" size="sm" />
+                    <q-icon name="delete_sharp" size="sm" />
+                </div>
+            </div>
             <p>{{ price }}</p>
         </div>
 
         <div class="entry-content">
-            <p>{{ frequency }}</p>
+            <p>{{ category }}</p>
             <p>{{ date }}</p>
         </div>
     </div>
 </template>
 
 <script setup>
-const props = defineProps(['title', 'price', 'frequency', 'date']);
+const props = defineProps(['description', 'price', 'category', 'date']);
 </script>
 
 <style scoped>
 .entry {
     background-color: #f0f0f0;
-    padding: 0.5rem;
+    padding: 0.25rem 0.5rem;
     margin: 1rem;
     border-radius: 0.5rem;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -28,6 +37,18 @@ const props = defineProps(['title', 'price', 'frequency', 'date']);
         display: flex;
         justify-content: space-between;
         align-items: center;
+
+        .description {
+            display: flex;
+            align-items: center;
+            gap: 6rem;
+
+            .actions {
+                display: flex;
+                gap: 0.5rem;
+                cursor: pointer;
+            }
+        }
 
         h2 {
             margin: 0;
