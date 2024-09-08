b54ee9409338666b70de005b63236328040a4b5c
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-08 20:54:34 +0300

    Modify model
    - include check for existing category
    - include expense relationship

--- backend/v1/models/category.js
+++ backend/v1/models/category.js
@@ -1,11 +1,31 @@
-const {Model, Schema} = require('mongoose');
+const { model, Schema } = require('mongoose');
 
 const categorySchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
     name: {
         type: String,
         required: true
-    }
+    },
+    expenses: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Expense'
+        }
+    ],
 });
 
-const category = Model('Category', categorySchema);
-module.exports = category;
+categorySchema.pre('save', async function (next) {
+    const category = this;
+    const existingCategory = await Category.findOne({userId: category.userId, name: category.name});
+    if (existingCategory) {
+        const error = new Error(`Category already exists`);
+        return next(error);
+    }
+    next();
+});
+
+const Category = model('Category', categorySchema);
+module.exports = Category;
