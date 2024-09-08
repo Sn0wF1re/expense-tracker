a54e554d4d249b17c7a1566c78216422c02c4687
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-08 20:58:56 +0300

    Page to display categories

---
+++ frontend/src/pages/CategoriesPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="budget">
+        <AddBudgetForm />
+    </div>
+
+    <div class="categories">
+        <AddCategoryForm />
+    </div>
+
+    <div class="entries">
+        <h2>Expense Categories</h2>
+        <CategoryEntry name="Fuel" date="2022-01-01" />
+    </div>
+</template>
+
+<script setup>
+import AddCategoryForm from '../components/AddCategoryForm.vue';
+import AddBudgetForm from '../components/AddBudgetForm.vue';
+import CategoryEntry from '../components/CategoryEntry.vue';
+</script>
+
+<style scoped>
+.entries {
+    h2 {
+        margin: 1.2rem;
+    }
+}
+</style>
