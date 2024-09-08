15e8c6c7159ea09447724875684c8a6e62c28a79
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-08 23:13:34 +0300

    Create expenses store for expense related tasks

--- frontend/src/stores/expensesStore.js
+++ frontend/src/stores/expensesStore.js
@@ -1,6 +1,106 @@
 import { defineStore } from 'pinia';
+import { LocalStorage } from 'quasar';
 import { ref } from 'vue';
 
 export const useExpensesStore = defineStore('expenses', () => {
     const expenses = ref([]);
+    const expense = ref({});
+    const token = LocalStorage.getItem('token');
+    const apiUrl = 'http://localhost:3000/api/v1';
+
+    const addExpense = async (expense) => {
+        try {
+            const response = await fetch(`${apiUrl}/expenses`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify(expense),
+            });
+            if (!response.ok) {
+                throw new Error('Error adding expense');
+            }
+            await fetchExpenses();
+        } catch (error) {
+            console.log('Error adding expense', error);
+        };
+    };
+
+    const fetchExpenses = async () => {
+        try {            
+            const response = await fetch(`${apiUrl}/expenses`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Error fetching expenses');
+            }
+            expenses.value = await response.json();
+        } catch (error) {
+            console.log('Error fetching expenses', error);
+        };
+    };
+
+    const fetchExpense = async (id) => {
+        try {
+            const response = await fetch(`${apiUrl}/expenses/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Error fetching expense');
+            }
+            expense.value = await response.json();
+        } catch (error) {
+            console.log('Error fetching expense', error);
+        };
+    };
+
+    const updateExpense = async (expense) => {
+        try {
+            const response = await fetch(`${apiUrl}/expenses/${expense.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify(expense),
+            });
+            if (!response.ok) {
+                throw new Error('Error updating expense');
+            }
+            await fetchExpenses();
+        } catch (error) {
+            console.log('Error updating expense', error);
+        };
+    };
+
+    const deleteExpense = async (id) => {
+        try {
+            const response = await fetch(`${apiUrl}/expenses/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Error deleting expense');
+            }
+            await fetchExpenses();
+        } catch (error) {
+            console.log('Error deleting expense', error);
+        };
+    };
+
+    return {
+        expenses,
+        addExpense,
+        fetchExpenses,
+        fetchExpense,
+        deleteExpense,
+        updateExpense,
+    };
 });
