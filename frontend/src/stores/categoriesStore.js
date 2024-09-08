332ce4b974ef8ab94b1b6749769ff05779c5768c
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-08 23:14:20 +0300

    Create store for categories-related tasks

---
+++ frontend/src/stores/categoriesStore.js
@@ -0,0 +1,69 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { LocalStorage } from "quasar";
+
+export const useCategoriesStore = defineStore("categories", () => {
+    const categories = ref([]);
+    const category = ref({});
+    const token = LocalStorage.getItem("token");
+    const apiUrl = "http://localhost:3000/api/v1";
+
+    const addCategory = async (category) => {
+        try {
+            const response = await fetch(`${apiUrl}/categories`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: JSON.stringify(category),
+            });
+            if (!response.ok) {
+                throw new Error("Error adding category");
+            }
+            await fetchCategories();
+        } catch (error) {
+            console.log("Error adding category", error);
+        };
+    };
+
+    const fetchCategories = async () => {
+        try {
+            const response = await fetch(`${apiUrl}/categories`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error("Error fetching categories");
+            }
+            categories.value = await response.json();
+        } catch (error) {
+            console.log("Error fetching categories", error);
+        };
+    };
+
+    const fetchCategory = async (id) => {
+        try {
+            const response = await fetch(`${apiUrl}/categories/${id}`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error("Error fetching category");
+            }
+            category.value = await response.json();
+        } catch (error) {
+            console.log("Error fetching category", error);
+        };
+    };
+
+    return {
+        categories,
+        category,
+        addCategory,
+        fetchCategories,
+        fetchCategory,
+    };
+});
