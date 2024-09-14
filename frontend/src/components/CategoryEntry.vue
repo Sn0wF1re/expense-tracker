f951514e71e5165a2b0e7cb14e9c6ec64f763d37
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-14 17:57:50 +0300

    Modify category entry card
    - include router link to display expenses for a category
    - List categories dynamically

--- frontend/src/components/CategoryEntry.vue
+++ frontend/src/components/CategoryEntry.vue
@@ -1,21 +1,23 @@
 <template>
-
-    <div class="entry">
-        <div class="description">
-            <h2>
-                {{ category.name }}
-            </h2>
-            <!-- <div class="actions">
-                <q-icon name="edit_sharp" size="sm" />
-                <q-icon name="delete_sharp" size="sm" />
-            </div> -->
+    <router-link :to="{ name: 'category', params: { categoryId: category._id} }">
+        <div class="entry">
+            <div class="description">
+                <h2>
+                    {{ category.name }}
+                </h2>
+                <!-- <div class="actions">
+                    <q-icon name="edit_sharp" size="sm" />
+                    <q-icon name="delete_sharp" size="sm" />
+                </div> -->
+            </div>
+    
+            <div class="entry-content text-positive">
+                <p v-if="category.expenses.length === 1">1 expense</p>
+                <p v-else-if="category.expenses.length === 0">0 expenses</p>
+                <p v-else>{{ category.expenses.length }} expenses</p>
+            </div>
         </div>
-
-        <div class="entry-content">
-            <p v-if="category.expenses.length === 1">1 expense</p>
-            <p v-else>{{ category.expenses.length }} expenses</p>
-        </div>
-    </div>
+    </router-link>
 </template>
 
 <script setup>
@@ -28,11 +30,15 @@ const props = defineProps({
 </script>
 
 <style scoped>
+a {
+    text-decoration: none;
+    color: inherit;
+}
 .entry {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #f0f0f0;
+    background-color: #fff;
     padding: 0.25rem 0.5rem;
     margin: 1rem;
     border-radius: 0.5rem;
@@ -57,7 +63,7 @@ const props = defineProps({
     }
     p {
         margin: 0;
-        color: #E06336;
+        /* color: #FFA259; */
     }
 
     .entry-content {
