2100151f253856c7e91d78bbd1b7287ae3c965a1
Sn0wF1re <mokayaclinton34@gmail.com>
2024-09-14 18:03:18 +0300

    Modify layout
    - include wrapper for all elements
    - wrap router view in a div
    - restrict the footer to the bottom of the page

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,19 +1,23 @@
 <template>
-    <header class="header">
-        <h2>WapiDoh</h2>
-
-        <nav class="navigation">
-            <router-link to="/expenses">Expenses</router-link>
-            <router-link to="/categories">Categories</router-link>
-        </nav>
-        <a class="logout" @click="logout">Log Out</a>
-    </header>
-
-    <router-view />
-
-    <footer class="footer">
-        <p>&copy; {{ date }} WapiDoh</p>
-    </footer>
+    <div class="wrapper">
+        <header class="header">
+            <h2>WapiDoh</h2>
+    
+            <nav class="navigation">
+                <router-link to="/expenses">Expenses</router-link>
+                <router-link to="/categories">Categories</router-link>
+            </nav>
+            <a class="logout" @click="logout">Log Out</a>
+        </header>
+    
+        <div class="content">
+            <router-view />
+        </div>
+    
+        <footer class="footer">
+            <p>&copy; {{ date }} WapiDoh</p>
+        </footer>
+    </div>
 </template>
 
 <script setup>
@@ -27,35 +31,61 @@ const logout = () => {
 </script>
 
 <style scoped>
-.header {
+.wrapper {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 1rem;
-    margin-bottom: 0;
-    padding: 0.5rem;
-
-    .navigation {
+    flex-direction: column;
+    min-height: 100vh;
+    
+    .header {
         display: flex;
-        gap: 1rem;
+        justify-content: space-between;
+        align-items: center;
+        margin: 1rem;
+        margin-bottom: 0;
+        padding: 0.5rem;
+    
+        .navigation {
+            display: flex;
+            gap: 1rem;
+    
+            a {
+                color: #000;
+                text-decoration: none;
+                cursor: pointer;
+            }
+    
+            a:hover {
+                color: #e6b272;
+            }
+        }
+    
+        .logout {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    
+        .logout:hover {
+            color: #e6b272;
+        }
+    }
+    
+    .content {
+        flex: 1;
     }
 
-    .logout {
-        color: #E06336;
-        text-decoration: none;
-        cursor: pointer;
-    }
-}
-
-.footer {
-    display: flex;
-    justify-content: center;
-    margin: 1rem;
-    padding: 1rem 0.5rem;
-    /* border-top: 1px solid #E06336; */
-
-    p {
-        margin: 0;
+    .footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 1rem;
+        padding: 1rem 0.5rem;
+        min-height: 10vh;
+        /* border-top: 1px solid #E06336; */
+    
+        p {
+            margin: 0;
+        }
     }
 }
 </style>
