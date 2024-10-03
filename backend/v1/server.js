7763eb52b6ff45d643ad3156f25230d50411aab1
Sn0wF1re <mokayaclinton34@gmail.com>
2024-10-03 11:23:31 +0300

    Allow accessin credentials from all origins

--- backend/v1/server.js
+++ backend/v1/server.js
@@ -10,9 +10,15 @@ dotenv.config();
 
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 
