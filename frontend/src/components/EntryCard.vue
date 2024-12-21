d3ecd78130b1589a9392c3ca8f6d8b83f19cccb9
Sn0wF1re <mokayaclinton34@gmail.com>
2024-12-21 16:55:11 +0300

    Modify how expenses display on small screens

--- frontend/src/components/EntryCard.vue
+++ frontend/src/components/EntryCard.vue
@@ -2,7 +2,7 @@
     <div class="entry">
         <div class="entry-header">
             <div class="description">
-                <h2>
+                <h2 class="description-text">
                     {{ entry.description }}
                 </h2>
 
@@ -12,6 +12,21 @@
                 </div>
             </div>
             <p class="price text-negative">- Kes {{ entry.amount }}</p>
+        </div>
+
+        <div class="mobile-entry-header">
+            <h2 class="description-text">
+                {{ entry.description }}
+            </h2>
+
+            <div class="mobile-description">
+                <div class="mobile-actions">
+                    <q-icon name="edit_sharp" size="sm" />
+                    <q-icon name="delete_sharp" size="sm" />
+                </div>
+
+                <p class="price text-negative">- Kes {{ entry.amount }}</p>
+            </div>
         </div>
 
         <div class="entry-content">
@@ -41,12 +56,17 @@ const formattedDate = computed(() => {
 </script>
 
 <style scoped>
+
 .entry {
     background-color: #fff;
     padding: 0.25rem 0.5rem;
     margin: 1rem;
     border-radius: 0.5rem;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+    .mobile-entry-header {
+        display: none;
+    }
 
     .entry-header {
         display: flex;
@@ -79,6 +99,41 @@ const formattedDate = computed(() => {
         justify-content: space-between;
         align-items: center;
         color: #666;
+    }
+}
+
+@media (max-width: 850px) {
+    .entry {
+        .entry-header {
+            display: none;
+        }
+
+        .mobile-entry-header {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+
+            .mobile-description {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                gap: 6rem;
+
+                .mobile-actions {
+                    display: flex;
+                    cursor: pointer;
+                }
+            }
+
+            h2 {
+                margin: 0;
+            }
+
+            p {
+                margin: 0;
+            }
+        }
     }
 }
 </style>
